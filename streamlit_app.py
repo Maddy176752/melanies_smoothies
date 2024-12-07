5957b57edea34c21a5706998479b8db7b14b98e6
Maddy176752 <manish.dabas@genpact.com>
2024-12-07 13:07:25 +0530

    Update streamlit_app.py
    added 2 statements in fruit chose block

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -33,6 +34,8 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string=ingredients_string+fruit_chosen+' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -50,7 +53,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         #st.success('Your Smoothie is ordered!', icon="✅")
         st.success("Your Smoothie is ordered"+ name_on_order, icon="✅")
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-st_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+
